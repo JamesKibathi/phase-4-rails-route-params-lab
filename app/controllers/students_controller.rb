2bbf1b575b6508aea45f22f03cbdf5ce75324a6c
James Njenga <knjenga94@gmail.com>
2022-11-24 00:50:08 +0300

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,21 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+ 
+
+  def show
+    student=Student.find(params[:id])
+    render json: student
+  end
+
+  def grades
+   grades=Student.all
+   render json: grades, only: [:grade]
+  end
+
+  def highest_grade
+    highest=Student.order("grade DESC").first
+    render json: highest
+  end
 
 end
